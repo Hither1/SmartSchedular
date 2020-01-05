cf4415e298c430260e9672d34111a9dafbccdd3b
Hither1 <suhither@gmail.com>
2020-01-05 11:47:41 +0800

    Add main page& start page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:ews/Http/WebCredentials.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:ews/ews.dart';
@@ -12,6 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
+    final service = ExchangeService.withVersion(ExchangeVersion.Exchange2007_SP1)
+      ..Url = Uri.parse("https://outlook.office365.com/ews/exchange.asmx")
+      ..Credentials = WebCredentials("---USER_NAME---", "---USER_PASSWORD---", "---USER_DOMAIN---");
+
+
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
---
+++ lib/start_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class StartScreen extends StatefulWidget{
+  @override
+  _StartScreenState createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen>{
+  @override
+  Widget build(BuildContext context){
+    return Container(
+
+    );
+  }
+}
